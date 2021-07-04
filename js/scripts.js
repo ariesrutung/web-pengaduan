b110aff2d841ea3b13abf76a011b14dc9f8176b0
ariesrutung <ariesrutung@gmail.com>
2021-07-04 18:21:44 +0900

    Fix masalah header

--- js/scripts.js
+++ js/scripts.js
@@ -34,15 +34,3 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 
-// JS untuk counter
-$('.counter-count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 5000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
