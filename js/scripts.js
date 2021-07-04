4475dd7ff78d2d4e21c47d2cbb40becb6a3815fb
ariesrutung <ariesrutung@gmail.com>
2021-07-04 16:29:52 +0900

    Mengatur Counter

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,17 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+// JS untuk counter
+$('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
